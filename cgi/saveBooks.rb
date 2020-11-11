0887f42c6dbf57b164f9c44b060c8dbc9fab5797
email <b1017216@gmail.com>
2020-11-11 17:34:26 +0900

    とりあえずひな形制作

---
+++ cgi/saveBooks.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+# -*- coding: utf-8 -*-
+
+require 'mysql2'
+require 'cgi'
+
+data=CGI.new()
+
+id = data['id']
+animal = data['animal']
+
+# セキュリティ面からデータベースのパス等は消しています　サーバー内で入力してください
+# client =
