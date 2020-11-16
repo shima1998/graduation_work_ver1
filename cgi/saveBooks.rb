5419a6c45488105f3fc124974f9935466bb90a06
email <b1017216@gmail.com>
2020-11-16 14:19:58 +0900

    あとアップロードしてテスト

--- cgi/saveBooks.rb
+++ cgi/saveBooks.rb
@@ -6,9 +6,34 @@ require 'cgi'
 
 data=CGI.new()
 
-id = data['id']
-animal = data['animal']
+name = data['book_name']
+status = data['book_status']
+reviewName = data['book_review_name']
+reviewPoint = data['book_review']
+impressions = data['book_impression']
 
 # セキュリティ面からデータベースのパス等は消しています　サーバー内で入力してください
 # client =
 
+print <<-EOS
+"Content-type: text/html\n\n"
+'<!DOCTYPE html><html lang="ja">'
+'<head>'
+'<title>InsertTest1</title>'
+'<meta charset="UTF-8" />'
+'</head>'
+'<body>'
+'<p>'
+EOS
+
+client.query("insert into test_book values(#{book_name} , '#{book_status}', '#{book_review_name}', '#{book_review}', '#{book_impression}');")
+
+product_results.each do |product_results|
+    puts product_results
+end
+
+print <<-EOS
+'</p>'
+'</body>'
+'</html>'
+EOS
