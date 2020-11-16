ec181ce171ec3bdc69e3c4eae5ce012a1debf7f6
shima1998 <b1017216@gmail.com>
2020-11-17 04:56:37 +0900

    データの挿入機能完了。クォーテーションの有無や改行コードに足を引っ張られました

--- cgi/saveBooks.rb
+++ cgi/saveBooks.rb
@@ -12,28 +12,31 @@ reviewName = data['book_review_name']
 reviewPoint = data['book_review']
 impressions = data['book_impression']
 
-# セキュリティ面からデータベースのパス等は消しています　サーバー内で入力してください
-# client =
+#パスはサーバーで入れてね
+# client = 
+
+product_results = client.query("select * from test_book;")
 
 print <<-EOS
-"Content-type: text/html\n\n"
-'<!DOCTYPE html><html lang="ja">'
-'<head>'
-'<title>InsertTest1</title>'
-'<meta charset="UTF-8" />'
-'</head>'
-'<body>'
-'<p>'
+Content-type: text/html\n\n
+<!DOCTYPE html><html lang="ja">
+<head>
+<title>InsertTest1</title>
+<meta charset="UTF-8" />
+</head>
+<body>
+<p>
 EOS
 
-client.query("insert into test_book values(#{book_name} , '#{book_status}', '#{book_review_name}', '#{book_review}', '#{book_impression}');")
+client.query("insert into test_book values('#{name}', #{status}, '#{reviewName}', #{reviewPoint}, '#{impressions}');")
 
 product_results.each do |product_results|
-    puts product_results
+   puts product_results
 end
 
 print <<-EOS
-'</p>'
-'</body>'
-'</html>'
+Test
+</p>
+</body>
+</html>
 EOS
