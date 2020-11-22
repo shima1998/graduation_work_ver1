2100c7922839be636bb91d321a81f5cd5fe0840f
shima1998 <b1017216@gmail.com>
2020-11-22 23:36:38 +0900

    show_book.rb参照 変数["colName"]で値だけ取れるらしい 文字化けと表示のおかしさを一気に解消

--- cgi/show_books.rb
+++ cgi/show_books.rb
@@ -17,8 +17,15 @@ print <<-EOS
 Content-type: text/html\n\n
 EOS
 
+# productResults.each do |productResults|
+#    puts CGI.unescape(productResults)
+# end
+
 productResults.each do |productResults|
-   puts productResults
+   print productResults["Name"] + "<br>\n"
+   print productResults["Status"].to_s + "<br>\n"
+   print productResults["ReviewName"] + "<br>\n"
+   print productResults["ReviewPoint"].to_s + "<br>\n"
+   print productResults["Inpressions"].to_s + "<br>\n"
+   print productResults["Date"].to_s + "<br>\n"
 end
-
-
